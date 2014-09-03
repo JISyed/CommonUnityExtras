1b5613fd03661a445ba0739603f68349bf5dead2
Jibran Ibrahim Syed <jisyed14@gmail.com>
2014-09-03 18:46:11 -0500

    Started initial Unity project

---
+++ CommonUnityExtrasProject/Assets/CUE_Examples/CUE_Logger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace CUE
+{
+	namespace TestingCode
+	{
+		public class CUE_Logger : MonoBehaviour 
+		{
+			void Start () 
+			{
+				Debug.Log("Is Windows: " 	+ CUE.System.Platform.IsWindows().ToString());
+				Debug.Log("Is Mac: " 		+ CUE.System.Platform.IsOSX().ToString());
+				Debug.Log("Is Linux: " 		+ CUE.System.Platform.IsLinux().ToString());
+				Debug.Log("Is Web Player: " + CUE.System.Platform.IsWebPlayer().ToString());
+			}
+
+			/*
+			void Update ()
+			{
+				
+			}
+			//*/
+		}
+	}
+}
---
+++ CommonUnityExtrasProject/Assets/CommonUnityExtras/System/Platform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace CUE
+{
+	namespace System
+	{
+		public static class Platform
+		{
+			public static bool IsWindows()
+			{
+				return Application.platform == RuntimePlatform.WindowsPlayer
+					|| Application.platform == RuntimePlatform.WindowsEditor
+					|| Application.platform == RuntimePlatform.WindowsWebPlayer;
+			}
+
+			public static bool IsOSX()
+			{
+				return Application.platform == RuntimePlatform.OSXPlayer
+					|| Application.platform == RuntimePlatform.OSXEditor
+					|| Application.platform == RuntimePlatform.OSXWebPlayer;
+			}
+
+			public static bool IsLinux()
+			{
+				return Application.platform == RuntimePlatform.LinuxPlayer;
+			}
+
+			public static bool IsWebPlayer()
+			{
+				return Application.platform == RuntimePlatform.WindowsWebPlayer
+					|| Application.platform == RuntimePlatform.OSXWebPlayer;
+			}
+		}
+	}
+}
