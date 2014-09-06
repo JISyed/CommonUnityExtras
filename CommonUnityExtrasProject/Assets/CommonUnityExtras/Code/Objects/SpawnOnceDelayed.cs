fd68979351112058f6994fcb1c2e47578d0adf2f
Jibran Ibrahim Syed <jisyed14@gmail.com>
2014-09-06 17:08:51 -0500

    Added PercentChance for calculating random outcomes and SpawnOnceDelayed for spawning prefabs after a certain time.

---
+++ CommonUnityExtrasProject/Assets/CommonUnityExtras/Code/Objects/SpawnOnceDelayed.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;	// Needed for IEnumerator
+
+namespace CUE
+{
+	namespace Objects
+	{
+		public class SpawnOnceDelayed : MonoBehaviour 
+		{
+			public float timeInSeconds;
+			public GameObject prefabToSpawn;
+			
+			// Use this for initialization
+			IEnumerator Start () 
+			{
+				// Wait for the assigned time
+				yield return new WaitForSeconds(timeInSeconds);
+				
+				// Spawn object
+				Instantiate(prefabToSpawn);
+				
+				// Self-destruct
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ CommonUnityExtrasProject/Assets/CommonUnityExtras/Code/System/Random.cs
@@ -0,0 +1,41 @@
+﻿
+namespace CUE
+{
+	namespace System
+	{
+		public static class Random 
+		{
+			public static bool PercentChance(int percentage)
+			{
+				if(percentage < 0)
+					return false;
+				
+				if(percentage > 100)
+					return true;
+				
+				int outcome = UnityEngine.Random.Range(1, 100);
+				
+				if(outcome <= percentage)
+					return true;
+				
+				return false;
+			}
+			
+			public static bool PercentChance(float percentage)
+			{
+				if(percentage <= 0.0f)
+					return false;
+				
+				if(percentage > 100.0f)
+					return true;
+				
+				float outcome = UnityEngine.Random.Range(0.0f, 100.0f);
+				
+				if(outcome <= percentage)
+					return true;
+				
+				return false;
+			}
+		}
+	}
+}
