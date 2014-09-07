ff8ed43866a84fe03780f7acee8ee94241ea4b72
Jibran Ibrahim Syed <jisyed14@gmail.com>
2014-09-06 22:25:28 -0500

    Added DestroyAfterTime, which simply destroys the attached object after a certain period of time in seconds

---
+++ CommonUnityExtrasProject/Assets/CommonUnityExtras/Code/Objects/DestroyAfterTime.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace CUE
+{
+	namespace Objects
+	{
+		public class DestroyAfterTime : MonoBehaviour 
+		{
+			public float timeInSeconds = 10.0f;
+
+			// Use this for initialization
+			void Start () 
+			{
+				if(timeInSeconds < 0.0f)
+				{
+					timeInSeconds = 0.0f;
+				}
+
+				Destroy(gameObject, timeInSeconds);
+			}
+
+		}
+	}
+}
