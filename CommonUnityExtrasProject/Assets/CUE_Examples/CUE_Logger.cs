826a9895950f687583de9f445a5c5e6d1ca1f403
Jibran Ibrahim Syed <jisyed14@gmail.com>
2014-09-06 22:27:07 -0500

    Updated example scene to test constant shader and new object scripts

--- CommonUnityExtrasProject/Assets/CUE_Examples/CUE_Logger.cs
+++ CommonUnityExtrasProject/Assets/CUE_Examples/CUE_Logger.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using TimeSpan = System.TimeSpan;
-using DateTime = System.DateTime;
 
 using CUE.DataType;
 
@@ -12,18 +10,7 @@ namespace CUE
 		{
 			void Start () 
 			{
-				TimeSpan time = DateTime.Now - DateTime.Today;
-				Debug.Log(time.FormattedClockString());
 
-				bool bTrue = "TRUE".ToBoolean();
-				bool bFalse = "false".ToBoolean();
-				bool yYes = "Yes".ToBoolean();
-				bool yNo = "nO".ToBoolean();
-
-				Debug.Log(bTrue.ToString());
-				Debug.Log(bFalse.ToString());
-				Debug.Log(yYes.ToString());
-				Debug.Log(yNo.ToString());
 			}
 
 			/*
